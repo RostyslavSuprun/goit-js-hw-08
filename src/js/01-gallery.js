a7456cafd62d2e50526dab6857e5f9519f00364b
RostyslavSuprun <kredlik62@gmail.com>
2022-08-19 14:59:08 +0300

    one more attempt

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import "simple-lightbox/dist/simpleLightbox.min.css"
+import SimpleLightbox from "simplelightbox"
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, description, original }) =>`<li class="gallery__link"><a class="gallery__item" href="${original}"><img class= "gallery__image" src=${preview} alt = "${description}"></a></li>`)
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});

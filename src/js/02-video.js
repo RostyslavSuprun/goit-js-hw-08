81aac500f0b844d0cf9a9c7728dcf0e8e97a1337
RostyslavSuprun <kredlik62@gmail.com>
2022-10-06 12:18:18 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle'
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+const getCurrentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+player.setCurrentTime(getCurrentTime);
+
+function onPlay({seconds}) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, seconds)
+}
+player.on('timeupdate', throttle(onPlay, 1000))
+
+player.on('play', function() {
+    console.log('played the video!');
+    console.log(getCurrentTime)
+    player.getCurrentTime().then(function (seconds) {
+    console.log(getCurrentTime)
+    // seconds = the current playback position
+}).catch(function(error) {
+    // an error occurred
+});
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle'
+
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const inputEl = document.querySelector('email');
+const textAreaEl = document.querySelector('message');
+
+form.addEventListener('input', throttle(saveMessage, 500))
+form.addEventListener("submit", submitForm);
+
+updateIntput();
+
+
+function saveMessage(evt) {
+  evt.preventDefault();
+  const { elements: { email, message } } = evt.currentTarget;
+  const formData = { email: email.value, message: message.value }
+  // console.log(formData)
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function submitForm(evt) {
+  evt.preventDefault();
+  const { elements: { email, message } } = evt.currentTarget;
+  const formData = { email: email.value, message: message.value }
+  console.log(formData)
+  localStorage.removeItem(LOCALSTORAGE_KEY)
+  form.reset();
+
+}
+
+function updateIntput() {
+  if (localStorage.getItem(LOCALSTORAGE_KEY) === null) {
+    return
+  }
+  auditInput = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+  form.elements.email.value = auditInput.email;
+  form.elements.message.value = auditInput.message
+}
 
